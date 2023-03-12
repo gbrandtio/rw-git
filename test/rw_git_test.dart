55f8511e7d791ff2cca631f9d0970b1bb41fa9c9
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-03-12 15:52:32 +0400

    - Fixing tests and naming conventions - Adding one more negative test

--- test/rw_git_test.dart
+++ test/rw_git_test.dart
@@ -2,16 +2,27 @@ import 'package:rw_git/rw_git.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
+  group('Ensures that', () {
     final rwGit = RwGit();
 
     test(
-        'The count of commits is zero if the given directory or tags do not exist',
+        'if the repository does not exist, the output contains only one INVALID entry',
         () async {
       List<String> commitsBetweenTags = await rwGit.getCommitsBetween(
           './extinct', 'v1.0.0_extinct', 'v1.0.1_extinct');
-      int countOfCommits = commitsBetweenTags.length;
 
+      expect(commitsBetweenTags[0], "INVALID");
+    });
+
+    test(
+        'the count of commits is zero if the given directory or tags do not exist',
+        () async {
+      List<String> commitsBetweenTags = await rwGit.getCommitsBetween(
+          './extinct', 'v1.0.0_extinct', 'v1.0.1_extinct');
+
+      // Remove the INVALID entries
+      commitsBetweenTags.removeWhere((element) => element == "INVALID");
+      int countOfCommits = commitsBetweenTags.length;
       expect(countOfCommits, 0);
     });
   });
