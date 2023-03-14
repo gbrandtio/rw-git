0358691aa59cb40d35c8fce686b16c245da2c527
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-03-14 20:53:02 +0400

    Delete rw_git_test default test file
    - Deleting default (generated) file for GIT in order to segregate unit tests by function.
    - Segregating the unit tests of getCommitsBetween(...) function into a separate file.

---
+++ test/get_commits_between_test.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'package:rw_git/rw_git.dart';
+import 'package:test/test.dart';
+
+final invalidResult = "INVALID";
+
+void main() {
+  late RwGit rwGit;
+
+  // Actions execution before every test.
+  setUp(() {
+    rwGit = RwGit();
+  });
+
+  /// Test group for [rwGit.getCommitsBetween()] function.
+  group('getCommitsBetween', () {
+    test('returns a List with one entry which is equal to INVALID', () async {
+      List<String> commitsBetweenTags = await rwGit.getCommitsBetween(
+          './extinct', 'v1.0.0_extinct', 'v1.0.1_extinct');
+
+      expect(commitsBetweenTags[0], invalidResult);
+    });
+
+    test(
+        'output length will be 0, if we do not take into consideration the INVALID entry',
+        () async {
+      List<String> commitsBetweenTags = await rwGit.getCommitsBetween(
+          './extinct', 'v1.0.0_extinct', 'v1.0.1_extinct');
+
+      commitsBetweenTags.removeWhere((element) => element == invalidResult);
+      int countOfCommits = commitsBetweenTags.length;
+      expect(countOfCommits, 0);
+    });
+  });
+}
