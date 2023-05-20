8a3569e10bea8dc24c4ed3b439c9031ce919b848
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-05-21 01:43:27 +0400

    Fixing refactored exports

--- lib/rw_git.dart
+++ lib/rw_git.dart
@@ -1,6 +1,6 @@
 /// A simple GIT wrapper.
 library rw_git;
 
-export 'src/git_service/rw_git_base.dart';
-export 'src/git_service/rw_git_parser.dart';
+export 'src/git_service/rw_git_facade.dart';
+export 'src/git_service/parsers/rw_git_parser.dart';
 export 'src/models/short_stat_dto.dart';
